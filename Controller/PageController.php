af96c49ce266cd85eb175a18dbcf9151dad17d5d
Maria Lansole <marialansole@gmail.com>
2011-11-17 01:39:59 +0000

    Initial funcionality added

---
+++ Controller/PageController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Lansole\PagesBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class PageController extends Controller
+{
+    /**
+     * Display Page
+     *
+     * @param string $path
+     */
+    public function indexAction($path)
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+
+        $page = $em->getRepository('LansolePagesBundle:Page')->findOneBy(array('path' => sprintf('/%s', $path)));
+
+        if (!$page && false === $this->get('security.context')->isGranted('ROLE_ADMIN')) {
+            throw $this->createNotFoundException('Unable to find page.');
+        } elseif (!$page && $this->get('security.context')->isGranted('ROLE_ADMIN')) {
+            
+
+            return $this->render('LansolePagesBundle:Page:new.html.twig', array('path' => $path));
+        }
+
+        return $this->render('LansolePagesBundle:Page:index.html.twig', array('page' => $page));
+    }
+}
---
+++ Entity/Page.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace Lansole\PagesBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM,
+    Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * @ORM\Entity(repositoryClass="Lansole\PagesBundle\Repository\PageRepository")
+ * @ORM\Table(name="page")
+ * @ORM\HasLifecycleCallbacks()
+ */
+class Page
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $title;
+
+    /**
+     * @Gedmo\Slug(fields={"title"})
+     * @ORM\Column(type="string", unique="true")
+     */
+    protected $slug;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $path;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $created_at;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    protected $updated_at;
+
+    /**
+     * Construct
+     */
+    public function __construct()
+    {
+        $this->setCreatedAt(new \DateTime());
+        $this->setUpdatedAt(new \DateTime());
+    }
+
+    /**
+     * @ORM\preUpdate
+     */
+    public function setUpdatedValue()
+    {
+       $this->setUpdatedAt(new \DateTime());
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set slug
+     *
+     * @param string $slug
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+    }
+
+    /**
+     * Get slug
+     *
+     * @return string 
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * Set created_at
+     *
+     * @param datetime $createdAt
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->created_at = $createdAt;
+    }
+
+    /**
+     * Get created_at
+     *
+     * @return datetime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * Set updated_at
+     *
+     * @param datetime $updatedAt
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updated_at = $updatedAt;
+    }
+
+    /**
+     * Get updated_at
+     *
+     * @return datetime 
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updated_at;
+    }
+
+    /**
+     * Set path
+     *
+     * @param string $path
+     */
+    public function setPath($path)
+    {
+        $this->path = $path;
+    }
+
+    /**
+     * Get path
+     *
+     * @return string 
+     */
+    public function getPath()
+    {
+        return $this->path;
+    }
+}
